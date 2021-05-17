7e8dcad87c6b4bf290fd8fb3f32f7e4e535f4a1c
lifegpc <g1710431395@gmail.com>
2021-05-17 20:24:49 +0800

    add file

---
+++ build.sh
@@ -0,0 +1,6 @@
+cp 0008-add-intl-to-pkg-file.patch packages/mingw-w64-fontconfig || exit 1
+cd packages || exit 1
+git apply --ignore-space-change --ignore-whitespace '../mingw-w64-fontconfig.patch' || exit 1
+cd mingw-w64-fontconfig || exit 1
+dos2unix PKGBUILD || exit 1
+MINGW_ARCH=mingw64 makepkg-mingw -sLf --nosign --noconfirm --skippgpcheck --noprogressbar --skipchecksums || exit 1
